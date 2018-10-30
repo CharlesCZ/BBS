d46f3cd2fbfad1b1ff31da483c62ee8b58d9279f
CharlesCZ <karakankaczyn@gmail.com>
2018-10-30 10:14:38 +0100

    dziala -Tocjent na Funkcja Carmichaela

--- src/Test.java
+++ src/Test.java
@@ -67,12 +67,14 @@ for(int j=0;j<key.size();++j) {
         if(  !(103<= seria[5] && seria[5] <= 209)  )
             return Boolean.FALSE;
 
-        for(int i=6;i<26;++i)
-        if(  !(103<= seria[i] && seria[i] <= 209)  ) {
-            System.out.println("serii "+i+ "  "+seria[i]);
+        for(int i=7;i<20000;++i)
+        seria[6]+=seria[i];
+        System.out.println("serii 6 i wiecej"+ "  "+seria[5]);
+        if(  !(103<= seria[6] && seria[6] <= 209)  )
             return Boolean.FALSE;
-        }
 
+
+//dla serii 6 i wiecej zsumowac
         if(  0< seria[26]  ) {
             System.out.println("dlugiej serii");
             return Boolean.FALSE;
